0eb7711dfe81002ec247d0c0f39560e5ca45cb37
rjzin <bigriann18@gmail.com>
2024-05-27 15:56:23 -0300

    Commit 2
    Codificados as entidades Livro e Editora.
    Ambos em typescript.

--- conhecendo_outro_framework/root/livros-angular/src/app/livro.ts
+++ conhecendo_outro_framework/root/livros-angular/src/app/livro.ts
@@ -1,2 +1,18 @@
 export class Livro {
+
+    // Declarando os campos
+    codigo: number;
+    codEditora: number;
+    titulo: string;
+    resumo: string;
+    autores: string[];
+
+    //
+    constructor(codigo:number, codEditora:number, titulo:string, resumo:string, autores:string[]) { 
+        this.codigo = codigo;
+        this.codEditora = codEditora;
+        this.titulo = titulo;
+        this.resumo = resumo;
+        this.autores = autores;
+    }
 }
