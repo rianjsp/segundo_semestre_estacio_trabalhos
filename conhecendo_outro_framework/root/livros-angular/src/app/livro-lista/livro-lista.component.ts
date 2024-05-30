d718741936c3993027fbffdf384299b5a845fef1
rjzin <bigriann18@gmail.com>
2024-05-30 13:46:35 -0300

    Update livro-lista.component.ts
    Codificado o componet Livro-Lista. Com os metodos ditos no sway.

--- conhecendo_outro_framework/root/livros-angular/src/app/livro-lista/livro-lista.component.ts
+++ conhecendo_outro_framework/root/livros-angular/src/app/livro-lista/livro-lista.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Editora } from '../editora';
+import { Livro } from '../livro';
+import { ControleEditoraService } from '../controle-editora.service';
+import { ControleLivrosService } from '../controle-livros.service';
 
 @Component({
   selector: 'app-livro-lista',
-  standalone: true,
-  imports: [],
   templateUrl: './livro-lista.component.html',
-  styleUrl: './livro-lista.component.css'
+  styleUrls: ['./livro-lista.component.css']
 })
-export class LivroListaComponent {
+export class LivroListaComponent implements OnInit {
 
+  public editoras: Array<Editora> = [];
+  public livros: Array<Livro> = [];
+
+  constructor(private servEditora: ControleEditoraService, private servLivros: ControleLivrosService) {}
+
+  ngOnInit(): void {
+    this.editoras = this.servEditora.getEditoras();
+    this.livros = this.servLivros.obterLivros();
+  }
+
+  excluir = (codigo: number) => {
+    this.servLivros.excluir(codigo);
+    this.livros = this.servLivros.obterLivros();
+  }
+
+  obterNome = (codEditora: number): string => {
+    return this.servEditora.getNomeEditora(codEditora);
+  }
 }
