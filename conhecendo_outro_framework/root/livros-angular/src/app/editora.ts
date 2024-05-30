09b300caac6cc977391ce4c8c50498f0d429be34
rjzin <bigriann18@gmail.com>
2024-05-30 13:05:48 -0300

    Update Entidades
    Construção das entidades, Livro e Editora.

--- conhecendo_outro_framework/root/livros-angular/src/app/editora.ts
+++ conhecendo_outro_framework/root/livros-angular/src/app/editora.ts
@@ -1,2 +1,13 @@
 export class Editora {
+
+  // Declaração das variaveis
+  codEditora: number;
+  nome: string;
+
+  // Construção
+  constructor(codEditora: number, nome: string) {
+    this.codEditora = codEditora;
+    this.nome = nome;
+  }
+
 }
--- conhecendo_outro_framework/root/livros-angular/src/app/livro.ts
+++ conhecendo_outro_framework/root/livros-angular/src/app/livro.ts
@@ -1,2 +1,19 @@
 export class Livro {
+
+  // Declaração das variaveis
+  codigo: number;
+  codEditora: number;
+  titulo: string;
+  resumo: string;
+  autores: string[];
+
+  // Construção
+  constructor(codigo: number, codEditora: number, titulo: string, resumo: string, autores: string[]) {
+    this.codigo = codigo;
+    this.codEditora = codEditora;
+    this.titulo = titulo;
+    this.resumo = resumo;
+    this.autores = autores;
+  }
+
 }
