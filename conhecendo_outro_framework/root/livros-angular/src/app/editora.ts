c09aa67d251e3bb95ed662ed0ad81dee82f9aa98
rjzin <bigriann18@gmail.com>
2024-05-30 12:59:32 -0300

    Update inicial
    Update inicial, para reestruturar o projeto.

--- conhecendo_outro_framework/root/livros-angular/src/app/editora.ts
+++ conhecendo_outro_framework/root/livros-angular/src/app/editora.ts
@@ -1,12 +1,2 @@
 export class Editora {
-
-    // Declaração dos campos
-    codEditora: number;
-    nome: string;
-
-    // 
-    constructor(codEditora: number, nome: string) { 
-        this.codEditora = codEditora;
-        this.nome = nome;
-    }
 }
