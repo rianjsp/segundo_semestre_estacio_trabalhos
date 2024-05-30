41a8b417a1c1fbbe750be7c99270cf022ab7f345
rjzin <bigriann18@gmail.com>
2024-05-30 13:17:04 -0300

    Update controle-editora.service.ts
    Codificado o controlador de editoras com os metodos getNome e getEditoras.

--- conhecendo_outro_framework/root/livros-angular/src/app/controle-editora.service.ts
+++ conhecendo_outro_framework/root/livros-angular/src/app/controle-editora.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Editora } from './editora'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ControleEditoraService {
 
+  editoras: Array<Editora> = [
+    {codEditora:1, nome:'Raios de Sol'},
+    {codEditora:2, nome:'Raios Lua'},
+    {codEditora:3, nome:'Emaranhado de estrelas'}
+  ]
+
   constructor() { }
+
+  getNomeEditora(codEditora: number): string {
+    const editorasEncontradas = this.editoras.filter(editora => editora.codEditora === codEditora)
+    if (editorasEncontradas.length > 0) {
+      return editorasEncontradas[0].nome;
+    } else {
+      return 'Editora não encontrada'
+    }
+  }
+
+  getEditoras(): Editora[] {
+    return this.editoras;
+   }
 }
